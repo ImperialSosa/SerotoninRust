02d2e5639f12119b60b4ba8059aae48317c28b7e
Harvey <dropc@DESKTOP-MR29OTV>
2023-09-25 21:00:06 +0200

    .

--- ultimate/Core/Hooking/ClientInput/ClientInput.cpp
+++ ultimate/Core/Hooking/ClientInput/ClientInput.cpp
@@ -47,12 +47,12 @@ bool TestFlying(AssemblyCSharp::BasePlayer* _This, Vector3 oldPos, Vector3 newPo
 			}
 			if (flag) {
 				float num5 = maxx((flyhackPauseTime > 0.f) ? 10 : 1.5, 0.f);
-				float num6 = _This->GetJumpHeight() + num5 + 3.8f;
+				float num6 = _This->GetJumpHeight() + num5 + 4.f;
 				if (flyhackDistanceVertical > num6) {
 					return true;
 				}
 				float num7 = maxx((flyhackPauseTime > 0.f) ? 10 : 1.5, 0.f);
-				float num8 = 5.f + num7 + 3.8f;
+				float num8 = 5.f + num7 + 4.f;
 				if (flyhackDistanceHorizontal > num8) {
 					return true;
 				}
@@ -95,7 +95,7 @@ bool CheckFlyhack(AssemblyCSharp::BasePlayer* _This, bool PreventFlyhack)
 					result = TestFlying(LocalPlayer, oldPos, modelPos);
 
 					float num5 = maxx((flyhackPauseTime > 0.f) ? 10 : 1.5, 0.f);
-					float num6 = _This->GetJumpHeight() + num5 + 3.8f;
+					float num6 = _This->GetJumpHeight() + num5 + 4.f;
 
 					m_settings::MaxVerticalFlyhack = num6;
 					m_settings::VerticalFlyhack = flyhackDistanceVertical;
@@ -103,7 +103,7 @@ bool CheckFlyhack(AssemblyCSharp::BasePlayer* _This, bool PreventFlyhack)
 					float num7 = maxx((flyhackPauseTime > 0.f) ? 10 : 1.5, 0.f);
 					float num8 = 5.f + num7;
 
-					m_settings::MaxHorisontalFlyhack = num8 + 3.8f;
+					m_settings::MaxHorisontalFlyhack = num8 + 4.f;
 					m_settings::HorisontalFlyhack = flyhackDistanceHorizontal;
 
 					if (result && PreventFlyhack)
